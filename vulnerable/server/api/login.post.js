b9d9a2b5923bcd96a7ea821adf91b0d9a87a1bc2
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-12-03 14:24:29 +0100

    🖖 vulnerability

--- vulnerable/server/api/login.post.js
+++ vulnerable/server/api/login.post.js
@@ -1,6 +1,6 @@
 import {PrismaClient} from '@prisma/client';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -8,11 +8,9 @@ export default defineEventHandler(async event => {
     const body = await readBody(event);
     const {email, password} = body;
 
-    const encrypted = bcrypt.hash(password, 10);
+    const [user] = await prisma.$queryRawUnsafe(`SELECT * FROM "User" WHERE email = '${email}'`);
 
-    const [user] = await prisma.$queryRawUnsafe(`SELECT * FROM "User" WHERE email = '${email}' AND password = '${encrypted}'`);
-
-    if (!user) {
+    if (!user || !bcrypt.compareSync(password, user.password)) {
         throw createError({statusCode: 401, statusMessage: 'Invalid user or password'});
     }
 
