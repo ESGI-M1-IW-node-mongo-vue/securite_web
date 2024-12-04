2227da98195f9fb1307d97e85b54deaeef005a8a
Lucas <ldp030602@gmail.com>
2024-12-04 13:24:39 +0100

    Retourner l'ensemble de l'utilisateur faille

--- vulnerable/server/api/me/[id].get.js
+++ vulnerable/server/api/me/[id].get.js
@@ -7,11 +7,6 @@ export default defineEventHandler(async event => {
     const id = getRouterParam(event, 'id')
 
     const user = await prisma.user.findUnique({
-        select: {
-            id: true,
-            name: true,
-            email: true
-        },
         where: {
             id: parseInt(id)
         }
